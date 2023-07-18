c8d465c4e477c8f37b7ccb98f5937a649c2cb026
SatooRu65536 <ryuto4619@gmail.com>
2023-07-18 14:05:27 +0900

    change class name to UpperCase

--- cmm.rb
+++ cmm.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class cmm < Formula
+class Cmm < Formula
   desc ""
   homepage ""
   url "https://github.com/SatooRu65536/cmm-lang/archive/refs/tags/1.0.1.tar.gz"
